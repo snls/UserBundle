c7b272db07e52c8268ddafd5ae0473dcdd99204f
looky_alba <looky.alba@gmail.com>
2016-07-20 16:19:52 +0300

    create user bundle for symfony 3

---
+++ Entity/User.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lookyalba
+ * Date: 20.07.16
+ * Time: 16:10
+ */
+
+namespace UserBundle\Entity;
+
+
+use Doctrine\ORM\Mapping\Entity as ORM;
+
+abstract class User
+{
+}
