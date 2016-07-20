3c7522ecf7e9f9d3d4ad1898a1d9f287ee3a2ca2
looky_alba <looky.alba@gmail.com>
2016-07-20 17:13:18 +0300

    add entities contact, personal, setting

---
+++ Entity/Contact.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lookyalba
+ * Date: 20.07.16
+ * Time: 17:08
+ */
+
+namespace UserBundle\Entity;
+
+
+class Contact
+{
+
+}
---
+++ Entity/Personal.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lookyalba
+ * Date: 20.07.16
+ * Time: 17:10
+ */
+
+namespace UserBundle\Entity;
+
+
+class Personal
+{
+
+}
