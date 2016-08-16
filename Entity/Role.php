ae7ac4e6ba9b18fcdfb803452f44c3117e2fdf03
looky_alba <looky.alba@gmail.com>
2016-08-16 19:53:31 +0300

    add entity Role

---
+++ Entity/Role.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lookyalba
+ * Date: 16.08.16
+ * Time: 19:41
+ */
+
+namespace UserBundle\Entity;
+
+use Symfony\Component\Security\Core\Role\RoleInterface;
+
+class Role implements RoleInterface
+{
+    public function getRole()
+    {
+        // TODO: Implement getRole() method.
+    }
+}
