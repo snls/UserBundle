cb7d131089644bd4be66676c87873685fca029d1
looky_alba <looky.alba@gmail.com>
2016-08-19 17:07:40 +0300

    Edit User entity and repository

--- Entity/User.php
+++ Entity/User.php
@@ -10,8 +10,122 @@ namespace UserBundle\Entity;
 
 
 use Doctrine\ORM\Mapping\Entity as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
-abstract class User implements UserInterface
+/**
+ * Class User
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ */
+class User
 {
+    /**
+     * @var $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=40, nullable=true, unique=true)
+     */
+    protected $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=40, nullable=true, unique=true)
+     */
+    protected $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=40, nullable=true)
+     */
+    protected $password;
+
+    /**
+     * Get ID
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set username
+     *
+     * @param $username
+     * @return $this
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set email
+     *
+     * @param $email
+     * @return $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set password
+     *
+     * @param $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+
+        return $this;
+    }
 }
